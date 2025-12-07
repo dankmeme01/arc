4a2ec9c08cc13a9dd85f2faa69eaa051f2bfc58a
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2025-12-07 02:56:59 +0100

    add way to override log function

--- include/arc/util/Trace.hpp
+++ include/arc/util/Trace.hpp
@@ -3,25 +3,35 @@
 #include <fmt/core.h>
 #include <fmt/std.h>
 #include <asp/time/Instant.hpp>
+#include <std23/move_only_function.h>
+
 #include <utility>
-#include <iostream>
 
 namespace arc {
 
 static auto epoch = asp::time::Instant::now();
 
+enum LogLevel {
+    Trace,
+    Error,
+};
+
+void doLogMessage(std::string message, LogLevel level);
+void setLogFunction(std23::move_only_function<void(std::string, LogLevel)> func);
+
 template <class... Args>
 void trace(fmt::format_string<Args...> fmt, Args&&... args) {
 #ifdef ARC_TRACE
     auto elapsed = epoch.elapsed();
-
-    fmt::println("[TRACE] [{:.4f}] {}", elapsed.seconds<float>(), fmt::format(fmt, std::forward<Args>(args)...));
+    auto message = fmt::format("[TRACE] [{:.4f}] {}", elapsed.seconds<float>(), fmt::format(fmt, std::forward<Args>(args)...));
+    doLogMessage(std::move(message), LogLevel::Trace);
 #endif
 }
 
 template <class... Args>
 void printError(fmt::format_string<Args...> fmt, Args&&... args) {
-    fmt::println(std::cerr, "[ERROR] {}", fmt::format(fmt, std::forward<Args>(args)...));
+    auto message = fmt::format("[ERROR] {}", fmt::format(fmt, std::forward<Args>(args)...));
+    doLogMessage(std::move(message), LogLevel::Error);
 }
 
 }
--- src/Util.cpp
+++ src/Util.cpp
@@ -1,10 +1,13 @@
 #include <arc/util/Random.hpp>
 #include <arc/util/Assert.hpp>
+#include <arc/util/Trace.hpp>
 #include <fmt/core.h>
 #include <random>
 #include <stdexcept>
+#include <iostream>
 
 namespace arc {
+
 
 std::array<uint64_t, 3> _getRandomSeed() {
     std::random_device rd;
@@ -20,6 +23,28 @@ std::array<uint64_t, 3> _getRandomSeed() {
 
 [[noreturn]] void _assertionFail(std::string_view what, std::string_view why, std::string_view file, int line) {
     throw std::runtime_error(fmt::format("Assertion failed ({}) at {}:{}: {}", what, file, line, why));
+}
+
+static std23::move_only_function<void(std::string, LogLevel)> g_logFunction;
+
+void doLogMessage(std::string message, LogLevel level) {
+    if (g_logFunction) {
+        g_logFunction(std::move(message), level);
+    } else {
+        switch (level) {
+            case LogLevel::Trace: {
+                fmt::println("[TRACE] {}", message);
+            } break;
+
+            case LogLevel::Error: {
+                fmt::println(std::cerr, "[ERROR] {}", message);
+            } break;
+        }
+    }
+}
+
+void setLogFunction(std23::move_only_function<void(std::string, LogLevel)> func) {
+    g_logFunction = std::move(func);
 }
 
 }
