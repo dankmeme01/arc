e84a346d7e0d6bc1619772eb0950218be3577db9
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2026-02-07 15:39:26 +0100

    inline yield/never impl

--- include/arc/task/Yield.hpp
+++ include/arc/task/Yield.hpp
@@ -5,28 +5,55 @@
 namespace arc {
 
 struct ARC_NODISCARD Yield : Pollable<Yield> {
-    bool poll(Context& cx);
+    bool poll(Context& cx) {
+        if (!yielded) {
+            yielded = true;
+            cx.wake();
+            return false;
+        } else {
+            return true;
+        }
+    }
 
 private:
     bool yielded = false;
 };
 
-Yield yield() noexcept;
+inline Yield yield() noexcept {
+    return Yield{};
+}
 
 struct ARC_NODISCARD Never : Pollable<Never> {
-    bool poll(Context& cx);
+    bool poll(Context& cx) {
+        return false;
+    }
 };
 
-Never never() noexcept;
+inline Never never() noexcept {
+    return Never{};
+}
 
 struct ARC_NODISCARD CoopYield : Pollable<CoopYield> {
-    bool poll(Context& cx);
+    bool poll(Context& cx) {
+        if (yielded) return true;
+
+        if (!cx.shouldCoopYield()) {
+            return true;
+        }
+
+        yielded = true;
+        cx.wake();
+
+        return false;
+    }
 
 private:
     bool yielded = false;
 };
 
 /// Yields if the current task has been running for way too long without yielding.
-CoopYield coopYield() noexcept;
+inline CoopYield coopYield() noexcept {
+    return CoopYield{};
+}
 
 }
