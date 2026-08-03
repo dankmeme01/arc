c0b1d79203c97f55891183bc817e2fd92f78aa2c
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2026-08-03 23:20:10 +0200

    allow overriding arc assert macros

--- include/arc/util/Assert.hpp
+++ include/arc/util/Assert.hpp
@@ -2,28 +2,33 @@
 #include <string_view>
 #include <utility>
 
-namespace arc {
-
 // Assert macro
-#define ARC_ASSERT(condition, ...) \
+#ifndef ARC_ASSERT
+# define ARC_ASSERT(condition, ...) \
     ARC__ASSERT2(condition, "" __VA_ARGS__)
 
-#define ARC__ASSERT2(condition, msg) \
+# define ARC__ASSERT2(condition, msg) \
     do { \
         if (!(condition)) [[unlikely]] { \
             ::arc::_assertionFail(#condition, msg, __FILE__, __LINE__); \
         } \
     } while (false)
-
-#if defined ARC_DEBUG
-# define ARC_DEBUG_ASSERT ARC_ASSERT
-#else
-# define ARC_DEBUG_ASSERT(...) (void)0
 #endif
 
-#define ARC_UNREACHABLE(msg) ARC_ASSERT(false, msg); std::unreachable()
+#ifndef ARC_DEBUG_ASSERT
+# if defined ARC_DEBUG
+#  define ARC_DEBUG_ASSERT ARC_ASSERT
+# else
+#  define ARC_DEBUG_ASSERT(...) (void)0
+# endif
+#endif
+
+#ifndef ARC_UNREACHABLE
+# define ARC_UNREACHABLE(msg) ARC_ASSERT(false, msg); std::unreachable()
+#endif
+
+namespace arc {
 
 [[noreturn]] void _assertionFail(std::string_view what, std::string_view why, std::string_view file, int line);
-
 
 }
